0265a3fadb9cc29c02d28f24537603209e303d42
Levani Birchadze <levanbirchadze@gmail.com>
2024-01-27 18:50:06 +0400

    fix sidebar

--- sidebar/script.js
+++ sidebar/script.js
@@ -1,5 +1,13 @@
 const sideBtn = document.querySelector(".sidebar-btn");
 const sidebar = document.querySelector(".sidebar");
+const closeBtn = document.querySelector(".close-btn");
+
 sideBtn.addEventListener("click", () => {
   sidebar.classList.toggle("show-sidebar");
+});
+
+closeBtn.addEventListener("click", () => {
+  if (sidebar.classList.contains("show-sidebar")) {
+    sidebar.classList.remove("show-sidebar");
+  }
 });
